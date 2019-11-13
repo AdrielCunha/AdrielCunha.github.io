a295e1201408b2686e34539263032bd5cdbf9e9d
adrielcunha <cunhaadriel@gmail.com>
2019-11-13 09:52:15 -0200

    color test

--- script.js
+++
@@ -1,5 +0,0 @@
-function play() {
-    const size = document.getElementById(`size`).value;
-    sessionStorage.setItem(`size`, size);
-    window.location.pathname.replace(`index.html`, `MazeGame/maze.html`);
-}
